32270472cfacf766ae8c7c8a6e0dd0678d73fd17
kethireddy krupadevi <kkrupadevi@gmail.com>
2017-11-08 23:22:05 +0530

    Add capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,7 @@
+
+set :stage, :production
+server '139.59.64.11', user: 'deploy', roles: %w{app db web}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
